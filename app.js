db53ccbf211e090adeafb9917b6ba84767838438
jackwsharples <jack.w.sharples@gmail.com>
2025-10-05 17:36:47 -0400

    getPings/getLatests and ride tracker scripts update

--- app.js
+++ app.js
@@ -143,3 +143,4 @@ app.listen(PORT, '0.0.0.0', () => {
 // Graceful shutdown
 process.on('SIGINT', async () => { await prisma.$disconnect(); process.exit(0); });
 process.on('SIGTERM', async () => { await prisma.$disconnect(); process.exit(0); });
+
